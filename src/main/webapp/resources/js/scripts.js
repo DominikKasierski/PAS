e05b2e8d6fe96b89a32d268e8084ea81ffa3051e
224323 Kasierski Dominik <dominek19@wp.pl>
2020-12-31 02:52:09 +0100

    Fighting with js walidation

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -1,13 +1,49 @@
 function showConfirmEmployment(text, number) {
     let r = confirm(text);
     if (r === true) {
-        document.getElementById('EmploymentListForm:j_idt39:'+number+':hdnBtn').click();
+        document.getElementById('EmploymentListForm:j_idt39:' + number + ':hdnBtn').click();
     }
 }
 
 function showConfirmBabysitter(text, number) {
     let r = confirm(text);
     if (r === true) {
-            document.getElementById('BabysitterListForm:j_idt39:'+number+':hdnBtn').click();
+        document.getElementById('BabysitterListForm:j_idt39:' + number + ':hdnBtn').click();
     }
+}
+
+function checkLogin(login) {
+    console.log(login);
+}
+
+function checkName(name) {
+    console.log(name);
+}
+
+function checkSurname(surname) {
+    console.log(surname);
+}
+
+function checkNumberOfChildren(number) {
+    console.log(number);
+}
+
+function checkAgeOfTheYoungestChild(age) {
+    console.log(age);
+}
+
+function checkAll1(login, name, surname) {
+    // checkLogin(login);
+    // checkName(imie);
+    // checkSurname(nazwisko);
+    //jesli wszystko git to sie wywoluje metoda z ukrytego, jak nie to nic
+}
+
+function checkAll2(login, name, surname, number, age) {
+    // checkLogin(login);
+    // checkName(imie);
+    // checkSurname(nazwisko);
+    // checkNumberOfChildren();
+    // checkAgeOfTheYoungestChild();
+    //jesli wszystko git to sie wywoluje metoda z ukrytego, jak nie to nic
 }
