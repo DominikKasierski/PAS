b64e654fe10f6e1c83af2224cbba4a7648abd198
224323 Kasierski Dominik <dominek19@wp.pl>
2020-12-31 12:32:12 +0100

    Still fighting with validation

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -12,39 +12,74 @@ function showConfirmBabysitter(text, number) {
     }
 }
 
-function checkLogin(login) {
-    console.log(login);
-}
-
-function checkName(name) {
-    console.log(name);
-}
-
-function checkSurname(surname) {
-    console.log(surname);
-}
-
-function checkNumberOfChildren(number) {
-    console.log(number);
-}
-
-function checkAgeOfTheYoungestChild(age) {
-    console.log(age);
-}
-
-function checkAll1(login) {
+function checkLogin(login, message) {
+    // TODO: SPRAWDZIC CZY NIE JEST PUSTY, SPRAWDZIC CZY MA MINIMUM 2 ZNAKI
     return false;
-    // checkLogin(login);
-    // checkName(imie);
-    // checkSurname(nazwisko);
-    //jesli wszystko git to sie wywoluje metoda z ukrytego, jak nie to nic
+    // if (poprawny) {
+    //     document.getElementById('loginDiv').innerHTML = "";
+    // } else {
+    //     document.getElementById('loginDiv').innerHTML = message;
+    //     return false;
+    // }
 }
 
-function checkAll2(login, name, surname, number, age) {
+function checkName(name, message) {
+    // TODO:SPRAWDZIC CZY NIE JEST PUSTY, SPRAWDZIC CZY MA MINIMUM 2 ZNAKI
+
+    if (poprawny) {
+        document.getElementById('nameDiv').innerHTML = "";
+    } else {
+        document.getElementById('nameDiv').innerHTML = message;
+        return false;
+    }
+}
+
+function checkSurname(surname, message) {
+    // TODO:SPRAWDZIC CZY NIE JEST PUSTY, SPRAWDZIC CZY MA MINIMUM 2 ZNAKI
+
+    if (poprawny) {
+        document.getElementById('surnameDiv').innerHTML = "";
+    } else {
+        document.getElementById('surnameDiv').innerHTML = message;
+        return false;
+    }
+}
+
+function checkNumberOfChildren(number, message) {
+    // TODO:SPRAWDZIC CZY JEST LICZBA I CZY DODATNIA
+
+    if (poprawny) {
+        document.getElementById('numberDiv').innerHTML = "";
+    } else {
+        document.getElementById('numberDiv').innerHTML = message;
+        return false;
+    }
+}
+
+function checkAgeOfTheYoungestChild(age, message) {
+    // TODO:SPRAWDZIC CZY JEST LICZBA I CZY Z ZAKRESU 0 - 15
+
+    if (poprawny) {
+        document.getElementById('ageDiv').innerHTML = "";
+    } else {
+        document.getElementById('ageDiv').innerHTML = message;
+        return false;
+    }
+}
+
+function checkAdminSuperUser() {
+    checkLogin(document.getElementById('j_idt37:login'), 'dupa');
+    // checkName(document.getElementById('nameDiv'));
+    // checkSurname(document.getElementById('surnameDiv'));
+    return true;
+}
+
+function checkClient() {
+    return false;
     // checkLogin(login);
     // checkName(imie);
     // checkSurname(nazwisko);
     // checkNumberOfChildren();
     // checkAgeOfTheYoungestChild();
-    //jesli wszystko git to sie wywoluje metoda z ukrytego, jak nie to nic
+    //jesli wszystko git to sie zwraca true, jak nie to false
 }
