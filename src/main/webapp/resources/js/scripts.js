4545a6e08cc60d6881cc217c5ce284d97932a40b
k-szczesniak <krzys.szcze@gmail.com>
2021-01-02 13:23:26 +0100

    JS validation for Babysitter

--- src/main/webapp/resources/js/scripts.js
+++ src/main/webapp/resources/js/scripts.js
@@ -27,8 +27,8 @@ function checkLogin(login, message) {
 function checkName(name, message) {
     let notEmpty = name !== '';
     let minLength = name.length > 2;
-    let onlyLetters = (/\d/.test(name));
-    if (notEmpty && minLength && !onlyLetters) {
+    let onlyLetters = (/^[A-Za-ząćęłńóśżźĄĆĘŁŃÓŚŻŹ]+$/.test(name));
+    if (notEmpty && minLength && onlyLetters) {
         document.getElementById('nameDiv').innerHTML = "";
         return true;
     } else {
@@ -40,8 +40,8 @@ function checkName(name, message) {
 function checkSurname(surname, message) {
     let notEmpty = surname !== '';
     let minLength = surname.length > 2;
-    let onlyLetters = (/\d/.test(name));
-    if (notEmpty && minLength && !onlyLetters) {
+    let onlyLetters = (/^[A-Za-ząćęłńóśżźĄĆĘŁŃÓŚŻŹ]+$/.test(surname));
+    if (notEmpty && minLength && onlyLetters) {
         document.getElementById('surnameDiv').innerHTML = "";
         return true;
     } else {
@@ -50,12 +50,12 @@ function checkSurname(surname, message) {
     }
 }
 
-function checkNumber(number, message, outputDiv) {
+function checkNumber(number, message, outputDiv, min, max) {
     let isNumber = /^\d+$/.test(number);
     let isInRange = false;
-    if(isNumber) {
+    if (isNumber) {
         let parseToInt = parseInt(number);
-        isInRange = parseToInt > 0 && parseToInt < 15
+        isInRange = parseToInt > min && parseToInt < max
     }
 
     if (isNumber && isInRange) {
@@ -79,4 +79,26 @@ function checkClient(msgLogin, msgName, msgSurname, msgNumOfChildren, msgAgeOfTh
     let numberOfChildrenCheck = checkNumber(document.getElementById('j_idt37:numberOfChildren').value, msgNumOfChildren, 'numberDiv');
     let ageCheck = checkNumber(document.getElementById('j_idt37:ageOfTheYoungestChild').value, msgAgeOfTheYoungestChild, 'ageDiv');
     return (loginNameSurnameCheck && numberOfChildrenCheck && ageCheck);
+}
+
+function checkBabysitter(msgName, msgSurname, msgBasePrice, msgMinChildAge, msgMaxNumOfChildren) {
+    let nameCheck = checkName(document.getElementById('j_idt37:name').value, msgName);
+    let surnameCheck = checkSurname(document.getElementById('j_idt37:surname').value, msgSurname);
+    let basePriceCheck = checkNumber(document.getElementById('j_idt37:basePriceForHour').value, msgBasePrice, 'basePriceDiv', 0, 1000);
+    let childAgeCheck = checkNumber(document.getElementById('j_idt37:minChildAge').value, msgMinChildAge, 'minChildAgeDiv', 0, 15);
+    let numChildrenCheck = checkNumber(document.getElementById('j_idt37:maximalNumberOfChildren').value, msgMaxNumOfChildren, 'maxNumOfChildDiv', 0, 15);
+    return (nameCheck && surnameCheck && basePriceCheck && childAgeCheck && numChildrenCheck);
+}
+
+function checkTeachingBabysitter(msgName, msgSurname, msgBasePrice, msgMinChildAge, msgMaxNumOfChildren, msgYearsOfExp) {
+    let basicPropCheck = checkBabysitter(msgName, msgSurname, msgBasePrice, msgMinChildAge, msgMaxNumOfChildren);
+    let yearsOfExpCheck = checkNumber(document.getElementById('j_idt37:yearsOfExperience').value, msgYearsOfExp, 'yearsDiv', 0, 70);
+    return (basicPropCheck && yearsOfExpCheck);
+}
+
+function checkTidingBabysitter(msgName, msgSurname, msgBasePrice, msgMinChildAge, msgMaxNumOfChildren, msgValueOfEquip) {
+    let basicPropCheck = checkBabysitter(msgName, msgSurname, msgBasePrice, msgMinChildAge, msgMaxNumOfChildren);
+    let valueOfEquipCheck = checkNumber(document.getElementById('j_idt37:valueOfCleaningEquipment').value, msgValueOfEquip, 'cleaningDiv', 0, 20000);
+    return (basicPropCheck && valueOfEquipCheck);
+
 }
