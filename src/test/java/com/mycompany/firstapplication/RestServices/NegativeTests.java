1b30eead39905a74727d5307f3c035d076c31bcc
224323 Kasierski Dominik <dominek19@wp.pl>
2021-01-24 12:46:48 +0100

    New tests and hints to the next added

--- src/test/java/com/mycompany/firstapplication/RestServices/NegativeTests.java
+++ src/test/java/com/mycompany/firstapplication/RestServices/NegativeTests.java
@@ -2,6 +2,8 @@ package com.mycompany.firstapplication.RestServices;
 
 import com.mycompany.firstapplication.Security.JWTGeneratorVerifier;
 import io.restassured.RestAssured;
+import io.restassured.http.Header;
+import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +13,7 @@ import java.net.URISyntaxException;
 import java.util.Arrays;
 import java.util.HashSet;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NegativeTests {
 
@@ -21,35 +22,110 @@ public class NegativeTests {
                     Arrays.asList("Admin"))));
 
     @Test
-    public void resourceValidationCheck() throws URISyntaxException {
-//        String JSON = "{\n" +
-//                "        \"basePriceForHour\": 123,\n" +
-//                "        \"maxNumberOfChildrenInTheFamily\": 5,\n" +
-//                "        \"minChildAge\": 1,\n" +
-//                "        \"name\": \"ZTatiana\",\n" +
-//                "        \"surname\": \"Alabaster\"\n" +
-//                "    }";
-//
-//        RequestSpecification requestAll = getBasicRequest();
-//        RequestSpecification requestPost = getBasicRequest();
-//
-//        requestPost.contentType("application/json");
-//        requestPost.body(JSON);
-//
-//        String getAllResponseString = requestAll.get(new URI("https://localhost:8181/PAS/rest/resources")).asString();
-//        int initialSize = getAllResponseString.split("},").length;
-//
-//        requestPost.post(new URI("https://localhost:8181/PAS/rest/resources/standard"));
-//
-//        getAllResponseString = requestAll.get(new URI("https://localhost:8181/PAS/rest/resources")).asString();
-//        assertEquals(initialSize + 1, getAllResponseString.split("},").length);
-//
-//        assertTrue(getAllResponseString.contains("\"name\":\"ZTatiana\""));
+    public void resourcesValidationCheck() throws URISyntaxException {
+        String JSON = "{\n" +
+                "        \"basePriceForHour\": -1,\n" +
+                "        \"maxNumberOfChildrenInTheFamily\": 5,\n" +
+                "        \"minChildAge\": 1,\n" +
+                "        \"name\": \"Anna\",\n" +
+                "        \"surname\": \"Panna\"\n" +
+                "    }";
+
+        RequestSpecification requestPost = getBasicRequest();
+        requestPost.contentType("application/json");
+        requestPost.body(JSON);
+
+        Response response = requestPost.post(new URI("https://localhost:8181/PAS/rest/resources/standard"));
+        assertEquals(422, response.statusCode());
+    }
+
+    @Test
+    public void usersValidationCheck() throws URISyntaxException {
+        String JSON = "{\n" +
+                "        \"name\": \"1\",\n" +
+                "        \"role\": \"Admin\",\n" +
+                "        \"password\": \"testTEST\",\n" +
+                "        \"surname\": \"testSurname\",\n" +
+                "        \"login\": \"testLogin\"\n" +
+                "    }";
+
+        RequestSpecification requestPost = getBasicRequest();
+        requestPost.contentType("application/json");
+        requestPost.body(JSON);
+
+        Response response = requestPost.post(new URI("https://localhost:8181/PAS/rest/users/admin"));
+        assertEquals(422, response.statusCode());
+    }
+
+    @Test
+    public void authorizationFailedLogin() throws URISyntaxException {
+        RequestSpecification requestPost = RestAssured.given();
+        requestPost.relaxedHTTPSValidation();
+        requestPost.contentType("application/json");
+
+        String JSON = "{\n" +
+                "        \"login\": \"nieMaTakiegoLoginu\",\n" +
+                "        \"password\": \"adamski\"\n" +
+                "    }";
+        requestPost.body(JSON);
+
+        Response responseFromAuth = requestPost.post(new URI("https://localhost:8181/PAS/rest/auth"));
+        assertEquals(401, responseFromAuth.statusCode());
+
+    }
+
+    @Test
+    public void authorizationFailedPassword() throws URISyntaxException {
+        RequestSpecification requestPost = RestAssured.given();
+        requestPost.relaxedHTTPSValidation();
+        requestPost.contentType("application/json");
+
+        String JSON = "{\n" +
+                "        \"login\": \"aAdamski\",\n" +
+                "        \"password\": \"nieMaTakiegoHasła\"\n" +
+                "    }";
+        requestPost.body(JSON);
+
+        Response responseFromAuth = requestPost.post(new URI("https://localhost:8181/PAS/rest/auth"));
+        assertEquals(401, responseFromAuth.statusCode());
+    }
+
+    @Test
+    public void authorizationFailedAccountActivity() throws URISyntaxException {
+        //1. wejsc na klienta, pokazac, ze autoryzacja zwrocila kod 202
+        //2. wejsc na admina, putem zmienic temu klientowi active na false
+        //3. sprobowac ponownie zalogowac się na admina, status = 401
+    }
+
+    @Test
+    public void employingEmployedBabysitter() throws URISyntaxException {
+        //1. wejsc na klienta, ktory ma jakies alokacje
+        //2. getem pobrac wszystkie alokacje i matchem wyciagnac z responsa uuid zatrudnionej opiekunnki
+        //3. sprobowac dodac zatrudnienie z tym uuid zwroci status 500 chyba?
+    }
+
+    @Test
+    public void httpsTest() throws URISyntaxException {
+        //1. sprobowac wyslac jakiegos geta uzywajac w URI http i 8080
+        //2. powinno zwrocic status (403?) = redirect
+    }
+
+    @Test
+    public void JWTTest() throws URISyntaxException {
+        //1. sprobowac wbic na jakis endpoint (oprocz auth) bez tokenu albo dodac naglowek, ale pusty
+        //2. powinno zwrocic status (401?) = unauthorized
+    }
+
+    @Test
+    public void roleTest() throws URISyntaxException {
+        //1. wbic z admina na jakis endpoint userow i pokazac, ze status 200
+        //2. sprobowac wbic na ten sam endpoint z clienta/superUsera i pokazac, ze status 403 forbidden
     }
 
     private RequestSpecification getBasicRequest() {
         RequestSpecification request = RestAssured.given();
         request.relaxedHTTPSValidation();
+        request.header(new Header("Authorization", "Bearer " + token));
         return request;
     }
 }
--- src/test/java/com/mycompany/firstapplication/RestServices/OptionalTests.java
+++ src/test/java/com/mycompany/firstapplication/RestServices/OptionalTests.java
@@ -14,9 +14,37 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class OptionalTests {
 
     @Test
-    public void getAllUsersTest() throws URISyntaxException {
-        RequestSpecification request = getBasicRequest();
+    public void updateToken() throws URISyntaxException {
+        //1. zalogowac sie, dodac token do naglowka, zapisac token do zmiennej, pokazac, ze mozna wejsc na jakis endpoint
+        //2. odswiezyc token, dodac go do naglowka, zapisac go do zmiennej, pokazac, ze mozna tez chodzic po endpointach
+        //3. pokazac, ze tokeny sa rozne
+        //4. przekonwertowac tokeny (JWTAuthenticationMechanism, linie 37 - 44)
+        //5. pokazac, ze odswiezony ma pozniejsza date wygasniecia
 
+    }
+
+    @Test
+    public void resourcesETag() throws URISyntaxException {
+        //1. zalogowac sie, pobrac ETag z geta, pobrac uuid jakiejs opiekunki
+        //2. utworzyc jakies nowe uuid, pokazac, ze sie rozni z tym co miala opiekunka
+        //3. sprobowac putnac ze zmienionym uuid, w naglowku If-Match = ETag
+        //4. status odpowiedzi = 400
+    }
+
+    @Test
+    public void usersETag() throws URISyntaxException {
+        //1. zalogowac sie, pobrac ETag z geta, pobrac uuid jakiegos usera
+        //2. utworzyc jakies nowe uuid, pokazac, ze sie rozni z tym co mial user
+        //3. sprobowac putnac ze zmienionym uuid, w naglowku If-Match = ETag
+        //4. status odpowiedzi = 400
+    }
+
+    @Test
+    public void updateTokenInactiveUser() throws URISyntaxException {
+        //1. wejsc na klienta, pokazac, ze autoryzacja zwrocila kod 202
+        //1. pokazac, ze endpoint updateToken zwraca status 202
+        //2. wejsc na admina, putem zmienic temu klientowi active na false
+        //3. sprobowac ponownie na kliencie odswiezyc token, status = 401
     }
 
     private RequestSpecification getBasicRequest() {
